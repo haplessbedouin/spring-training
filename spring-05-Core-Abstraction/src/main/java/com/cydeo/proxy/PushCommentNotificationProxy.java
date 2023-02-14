3adce5c8c106d3934ac784ecbe91eea307bf6966
Ahmet Tugrul <ahmedsaid0173@gmail.com>
2023-02-13 18:23:49 -0600

    @Qualifier annotation

--- spring-05-Core-Abstraction/src/main/java/com/cydeo/proxy/PushCommentNotificationProxy.java
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/proxy/PushCommentNotificationProxy.java
@@ -1,9 +1,11 @@
 package com.cydeo.proxy;
 
 import com.cydeo.model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("PUSH")
 public class PushCommentNotificationProxy implements CommentNotificationProxy{
 
 
