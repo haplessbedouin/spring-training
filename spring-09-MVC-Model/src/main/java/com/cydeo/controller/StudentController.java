55080526e219266eff6abba001fc3868da27f48e
Ahmet Tugrul <ahmedsaid0173@gmail.com>
2023-02-19 16:15:45 -0600

    Created different models using objects with thymeleaf

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,8 +1,14 @@
 package com.cydeo.controller;
 
+import com.cydeo.entity.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class StudentController {
@@ -11,7 +17,28 @@ public class StudentController {
     public String homePage(Model model){
         model.addAttribute("name", "Cydeo");
         model.addAttribute("course", "MVC");
-        model.addAttribute("subject", "Tugrul");
+
+        String subject = "Spring Boot";
+        model.addAttribute("subject", subject);
+
+        int studentId = new Random().nextInt();
+        model.addAttribute("id", studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(2);
+        numbers.add(4);
+        numbers.add(6);
+        numbers.add(8);
+        numbers.add(10);
+
+        model.addAttribute("numbers", numbers);
+
+        LocalDate dt = LocalDate.now();
+        model.addAttribute("date", dt);
+
+        Student student = new Student(1,"Said", "Tugrul");
+        model.addAttribute("student", student);
+
         return "student/welcome";
     }
 
