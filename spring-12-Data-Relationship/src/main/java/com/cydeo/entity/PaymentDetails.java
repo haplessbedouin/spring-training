3ade26346a37b762d254a566c9ddfb909345bd00
Ahmet Tugrul <ahmedsaid0173@gmail.com>
2023-04-03 18:25:43 -0500

    entities created

---
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/PaymentDetails.java
@@ -0,0 +1,29 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "paymentDetails")
+@Data
+@NoArgsConstructor
+public class PaymentDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal merchantPayoutAmount;
+    private BigDecimal commissionAmount;
+    @Column(columnDefinition = "DATE")
+    private LocalDate payoutDate;
+
+    public PaymentDetails(BigDecimal merchantPayoutAmount, BigDecimal commissionAmount, LocalDate payoutDate) {
+        this.merchantPayoutAmount = merchantPayoutAmount;
+        this.commissionAmount = commissionAmount;
+        this.payoutDate = payoutDate;
+    }
+}
