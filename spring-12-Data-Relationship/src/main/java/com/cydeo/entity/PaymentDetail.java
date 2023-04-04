4870eedcfc4bad962669cfb58baaafb098f075bb
Ahmet Tugrul <ahmedsaid0173@gmail.com>
2023-04-04 06:04:17 -0500

    uni/bi-directional

--- spring-12-Data-Relationship/src/main/java/com/cydeo/entity/PaymentDetail.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/PaymentDetail.java
@@ -20,6 +20,8 @@ public class PaymentDetail {
     private BigDecimal commissionAmount;
     @Column(columnDefinition = "DATE")
     private LocalDate payoutDate;
+    @OneToOne(mappedBy = "paymentDetail")
+    private Payment payment;
 
     public PaymentDetail(BigDecimal merchantPayoutAmount, BigDecimal commissionAmount, LocalDate payoutDate) {
         this.merchantPayoutAmount = merchantPayoutAmount;
