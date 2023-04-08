5bfbbb3c13f683a2a11b846631021d96d181e8dd
Ahmet Tugrul <ahmedsaid0173@gmail.com>
2023-04-08 17:40:55 -0500

    JPQL queries

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
@@ -25,6 +27,22 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     //Display all employees with salaries less than ""
     List<Employee> findBySalaryLessThan(Integer salaryLessThan);
+
+    //Display all employees that has been hired between "" and ""
+    List<Employee> findByHireDateBetween(LocalDate date1, LocalDate date2);
+
+    //Display all employees where salaries greater and equal to "" in order
+    List<Employee> findBySalaryGreaterThanEqualOrderBySalaryAsc(Integer salary);
+
+    //Display top unique 3 employees that is making less than ""
+    List<Employee> findDistinctTop3BySalaryLessThan(Integer salary);
+
+    //Display all employees that don't have email address
+    List<Employee> findByEmailIsNull();
+    @Query("select e from Employee e where e.email='amcnee1@google.es'")
+    Employee retrieveEmployeeDetail();
+    @Query("select e.salary from Employee e where e.email='amcnee1@google.es'")
+    Employee retrieveEmployeeSalary();
 
 
 
