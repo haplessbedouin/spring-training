04c4c902734dbe501aa07b348e7cfdc7877960c8
Ahmet Tugrul <ahmedsaid0173@gmail.com>
2023-05-07 14:29:59 -0500

    LoggingAspect examples

--- spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.cydeo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -13,12 +14,25 @@ public class LoggingAspect {
 
     Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Pointcut("execution(* com.cydeo.controller.CourseController.*(..))")
-    public void myPointcut(){}
+//    @Pointcut("execution(* com.cydeo.controller.CourseController.*(..))")
+//    public void myPointcut(){}
+//
+//    @Before("myPointcut()")
+//    public void log(){
+//        logger.info("Info log................");
+//    }
 
-    @Before("myPointcut()")
-    public void log(){
-        logger.info("Info log................");
+//    @Before("execution(* com.cydeo.controller.CourseController.*(..))") We can create point directly with advice but won't be able to use this pointcut with other advices
+//    public void log(){
+//        logger.info("Info log................");
+//    }
+
+    @Pointcut("execution(* com.cydeo.repository.CourseRepository.findById(*))")
+    public void courseRepositoryFindByIdPC(){}
+
+    @Before("courseRepositoryFindByIdPC()")
+    public void beforeCourseRepositoryFindById(JoinPoint joinPoint){
+        logger.info("Before -> Method: {}, Arguments: {}, Target: {}", joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
     }
 
 }
